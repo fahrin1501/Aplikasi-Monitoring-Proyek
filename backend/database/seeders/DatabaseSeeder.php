d03e02f0973046202f934b0c36ed43e5fda77874
Craticism <319951593+fahrin1501@users.noreply.github.com>
2026-09-15 18:06:29 +0800

    Feat: Menambahkan seeder akun Super Administrator

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,23 +3,24 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Menggunakan updateOrCreate agar tidak error duplikat jika di-seed berulang kali
+        User::updateOrCreate(
+            ['email' => 'superadministrator123@gmail.com'], // Kondisi pencarian
+            [
+                'name' => 'Super Administrator',
+                'password' => Hash::make('123456'),
+                // 'role' => 'admin', // (Buka komentar ini JIKA di tabel users Anda ada kolom 'role')
+            ]
+        );
     }
 }
