7a3b71b378ecbf8aae5bf0e999a0116ac96e0683
Craticism <319951593+fahrin1501@users.noreply.github.com>
2026-09-18 23:25:55 +0800

    fix rabi

--- frontend/src/pages/Projects/ProjectDetail/ProjectRAB.jsx
+++ frontend/src/pages/Projects/ProjectDetail/ProjectRAB.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import api from '../../../../api'; 
+import api from '../../../api'; 
 import { Loader2, ShieldAlert } from 'lucide-react';
 
 import NavigasiRAB from './Rab/NavigasiRAB';
